0e415ab74a7f7d2f8c7ff57d38266452385b5ae5
Akash padwal <akashpadwal21@gmail.com>
2024-07-23 00:31:46 +0530

    -feat * work experience added * basic styling added
    - TODO:
    * improvise overall styling
    * improvise code

---
+++ components/home/work.data.ts
@@ -0,0 +1,149 @@
+import { IWorkExp } from "./workExperience";
+
+export const WORK_EXPERIENCE: IWorkExp[] = [
+  {
+    companyName: "CodeB",
+    designation: "Software Engineer",
+    duration: {
+      from: "Dec 2021",
+      to: "Nov 2022",
+    },
+    projects: [
+      {
+        title: "Algo",
+        description: `Online application that makes hiring easy for HRs and selecting candidates based on the  online coding test.`,
+        stack: [
+          "ReactJS",
+          "MongoDB",
+          "AWS",
+          "NestJs",
+          "Scss",
+          "Bootstrap",
+          "Redux",
+        ],
+        contribution: [
+          "Create UI Dashboard, create compilation templates for reusability. Support adding new questions for various languages.  ",
+          "Code Optimisation and restructuring",
+          " Integrate camera module to take picture while taking the test and save the data on Aws S3",
+        ],
+      },
+      {
+        title: "CrowdNautics",
+        description: `Mobile application with 3 Lakhs+ downloads and daily active user base of 20k+ users.`,
+        stack: [
+          "React Native",
+          "Android",
+          "iOS",
+          "AWS",
+          "Firebase",
+          "MongoDB",
+          "NestJs",
+          "ReactJS",
+          "Ampitude",
+          "NewRelic",
+          "Scss",
+        ],
+        contribution: [
+          "Using Figma Designs create  apps from  scratch  and deploy on Play store and App Store.",
+          "Understanding the tools and services provided by Aws and create infrastructure using terraform on Aws",
+          "Setup CI CD for the backend deployment on AWS ECS, AWS Lambda and make infrastructure deployment for using terraform.",
+          "Integrate the apps with Firebase.",
+          "Create and deploy new schedulers on AWS Lambda service for push Notification, Payments, etc",
+          "Improvise the payment methods for data verification using data aggregation from the database and generate csv to verify the fraudulent user.",
+          "Optimise database indexing for better querying and faster results and stop data duplication and corruption.",
+          "Participate in Daily Scrum meeting with the client and understand the requirements and add new features to the application based on the requirements and verify backward compatibility.",
+          "Integrate NewRelic and Amplitude for application insights ",
+          "Create Admin Panel for better management",
+        ],
+      },
+      {
+        title: "Hbits",
+        description: "Online Platform for investing in real estate.",
+        stack: ["NextJS", "ReactJS", "AWS", "Hapijs", "NewRelic", "MongoDB"],
+        contribution: [
+          "Deploy the NextJs website on Vercel Cloud and connect it to Cloud fare for better availability and protection.",
+          "Optimise the SEO and reduce the loading time and improve the availability.",
+          "Create infrastructure on AWS using terraform and setup CICD pipelines.",
+          "Migrate the faulty Infrastructure to AWS.",
+          "Fix critical issues with the payment gateways.",
+          "Integrate Lead Squared CRM with the backend. ",
+        ],
+      },
+      {
+        title: "Eplanner",
+        description:
+          "Web Application used for management and organising big meetings and get user feedbacks.",
+        stack: ["ReactJS", "Azure", "Scss", "NestJs"],
+        contribution: [
+          "Create React Js application from the scratch replicating the Figma design and lead the frontend development.",
+          "Understanding the tools and services provided by Azure and create infrastructure on Azure using terraform and deploy the backend on Azure Container Apps and App Service.",
+          "Participate in Daily Scrum meeting with the client and understand the requirements.",
+          "Review the code, PR and integrate new features.",
+        ],
+      },
+      {
+        title: "Web Scrapper",
+        description: "",
+        stack: ["Nodejs", "Puppeteer"],
+        contribution: [
+          "Perform web scraping using Puppeteer and gather the data from specific web sites.",
+        ],
+      },
+    ],
+  },
+  {
+    companyName: "Doyen Infosolutions",
+    designation: "Software Engineer",
+    duration: {
+      from: "Nov 2022",
+      to: "Present",
+    },
+    projects: [
+      {
+        title: "AtlasCopco",
+        description:
+          "Work on a cluster of application that are interconnected and help in HR processes and used by the clients.",
+        stack: ["Angular", "NodeJs", "MongoDb", "Azure", "PostgreSQL"],
+        contribution: [
+          "Work on Idempotent event based application connectivity across cluster of applications.",
+          "Create scrips for data migration and correction.",
+          "Create new pages and components in Angular and deploy it on Pm2 ",
+          "Design new Api and add new features as asked by the client.",
+          "Fix critical bugs related to the code, database and querying without effecting the existing working of the application.",
+          "Improvise the existing features like mailing and help analyse the same from the frontend.",
+          "Optimise API for better segregation of data for better querying and low latency that fetches the data from SAP",
+          "Deploy applications on Azure App Service.",
+          "Verifying and Debugging the application Logs using Azure Log Stream.",
+        ],
+      },
+      {
+        title: "Montova",
+        description:
+          "Web Application used by big companies for logistics management on a very large scale.",
+        stack: ["ReactJs", "IIS", "JFrog"],
+        contribution: [
+          "Work on MicroFrontend and create separate modules.",
+          "Analysis for JFrog artifactory management and create new UI Library that can be reused access various application and deploy it on JFrog",
+          "Migrate application from AntD to MUI",
+          "Create generic and reusable components that can be used as a UI Library.",
+          "Integrate new libraries like react flow to better visualise the infrastructure, etc",
+        ],
+      },
+      {
+        title: "Auditry",
+        description:
+          "Web Application for helping companies simplify the auditing process.",
+        stack: ["ReactJs", "Cypress", "FusionAuth"],
+        contribution: [
+          "Dockerize the application.",
+          "Analyse the authentication management using FusionAuth and Containerizing the same.",
+          "Create new modules and  improvise the styling for the application.",
+          "Analysis for cypress and write code for automation end-to-end tests.",
+          "Working with backend developer and adding new features to the application providing better application insights.",
+          "Create components from scratch on top of AntD  components that don’t exists.",
+          "Deploy application on IIS server.",
+        ],
+      },
+    ],
+  },
+];
--- components/home/workExperience.tsx
+++ components/home/workExperience.tsx
@@ -1,35 +1,118 @@
-interface IWorkExp {
-  company: {
-    duration: {
-      from: string;
-      to: string;
-    };
-    projects: { stack: string[]; contribution: string[]; title: string }[];
+import { useState } from "react";
+import { WORK_EXPERIENCE } from "./work.data";
+import { DownArrow, UpArrow } from "@styled-icons/boxicons-solid";
+
+export interface IWorkExp {
+  companyName: string;
+  designation: string;
+  duration: {
+    from: string;
+    to: string;
   };
+  projects: {
+    title: string;
+    description: string;
+    stack: string[];
+    contribution: string[];
+  }[];
 }
 
 const WorkExperience = () => {
-  const workExp: IWorkExp[] = [];
+  const workExp: IWorkExp[] = WORK_EXPERIENCE;
+  return (
+    <div>
+      {workExp.map((w, i) => (
+        <ListItem
+          key={i}
+          id="company"
+          designation={w.designation}
+          companyName={w.companyName}
+          duration={w.duration}
+        >
+          <div
+            id="projects"
+            className="bg-white p-1 rounded-sm flex flex-col divide-y-2 divide-dashed"
+          >
+            {w.projects.map((p, i) => (
+              <div key={i} className="text-black">
+                <div key={i} className="mt-5">
+                  <span id="project" className="font-bold mr-1 text-lg">
+                    {p.title} -
+                  </span>
+                  <span
+                    id="description"
+                    className="text-sm tracking-wide italic"
+                  >
+                    {p.description}
+                  </span>
+                  <div id="stack">
+                    <span className="text-md font-bold">Tech - </span>
+                    <span>
+                      {p.stack.map((t, i) => (
+                        <span
+                          key={i}
+                          className="text-sm m-1 italic bg-black text-white px-2 py-0.5 rounded-md inline-block"
+                        >
+                          {t}
+                        </span>
+                      ))}
+                    </span>
+                  </div>
+                  <div id="contribution" className="mt-2">
+                    <ul className="list-disc">
+                      {p.contribution.map((c, i) => (
+                        <li
+                          key={i}
+                          className="ml-4 list-item text-sm tracking-wide"
+                        >
+                          {c}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </ListItem>
+      ))}
+    </div>
+  );
+};
+
+const ListItem: any = ({ children, designation, companyName, duration }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div className="text-white">
-      {workExp.map((w) => {
-        <div id="company">
-          {w.company}
-          {w.company.projects.map((p) => {
-            <div>
-              <div id="project">{p.title}</div>
-              <div id="stack">{p.stack}</div>
-              <div id="contribution">
-                <ul>
-                  {p.contribution.map((c) => {
-                    <li>{c}</li>;
-                  })}
-                </ul>
+      <div id="header" className="flex flex-col w-100 justify-between">
+        <div className="flex flex-row mb-10 justify-between">
+          <div id="title">
+            <div className="font-bold text-xl">{designation}</div>
+            <div className="flex flex-col">
+              <div className="font-semibold text-lg">{companyName}</div>
+              <div className="italic text-sm">
+                <span>
+                  {duration?.from} - {duration?.to}
+                </span>
               </div>
-            </div>;
-          })}
-        </div>;
-      })}
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <button onClick={() => setIsExpanded((prev) => !prev)}>
+              {!isExpanded ? (
+                <DownArrow className="w-5 h-5" />
+              ) : (
+                <UpArrow className="w-5 h-5" />
+              )}
+            </button>
+            <span className="text-xs">
+              {isExpanded ? `Collapse` : `Expand`}
+            </span>
+          </div>
+        </div>
+      </div>
+      {isExpanded ? children : <></>}
     </div>
   );
 };
